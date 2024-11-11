9f75fd4a7224a95e15e085c7e4992ea0fde9024e
Carolina Lopez <anacarito06@gmail.com>
2024-11-11 17:15:54 -0500

    Componentes ciclo de vida

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
+
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./component/slider/slider.component').then(m => m.SliderComponent)
+    },
+    {
+        path: 'card',
+        loadComponent: () => import('./component/card/card.component').then(m => m.CardComponent)
+    },
+
+    {
+        path: 'input',
+        loadComponent: () => import('./component/input/input.component').then(m => m.InputComponent)
+    }
+];
